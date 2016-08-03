5ee68c000a147e64bea52f297a9d0ee07e70cb69
Feross Aboukhadijeh <feross@feross.org>
2016-08-03 15:52:58 -0700

    comment

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-/**
- * Determine if an object is Buffer
+/*!
+ * Determine if an object is a Buffer
  *
- * Author:   Feross Aboukhadijeh <feross@feross.org> <http://feross.org>
- * License:  MIT
- *
- * `npm install is-buffer`
+ * @author   Feross Aboukhadijeh <feross@feross.org> <http://feross.org>
+ * @license  MIT
  */
 
 module.exports = function (obj) {
