3a709f844b2a77c7da5c2756ccc3dd71d36576b3
Feross Aboukhadijeh <feross@feross.org>
2018-01-29 15:37:06 +0800

    avoid casting object to boolean

--- index.js
+++ index.js
@@ -6,5 +6,5 @@
  */
 
 module.exports = function isBuffer (obj) {
-  return obj != null && !!obj.constructor && typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+  return obj != null && obj.constructor != null && typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
 }
