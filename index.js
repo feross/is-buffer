a0df32bd66780b7c223a7e8c81998ffb0e2cb304
Richie Bendall <richiebendall@gmail.com>
2020-07-06 03:19:02 +1200

    Use optional chaining and nullish coalescing

--- index.js
+++ index.js
@@ -6,6 +6,5 @@
  */
 
 module.exports = function isBuffer (obj) {
-  return obj != null && obj.constructor != null &&
-    typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+  return obj?.constructor?.isBuffer?.(obj) ?? false
 }
